a812b8ebf68cbdf1094eafd6d09c05a4f8c14490
Abhishek Tiwari <notabhishektiwari@gmail.com>
2025-02-24 12:44:24 +0000

    create chapter4/macros.h

---
+++ chapter4/macros.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+#define LIKELY(x) __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+
+inline auto ASSERT(bool cond, const std::string& msg) noexcept {
+    if(cond) [[unlikely]] {
+        std::cerr << msg << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+inline auto FATAL(const std::string& msg) noexcept {
+    std::cerr << msg << std::endl;
+    exit(EXIT_FAILURE);
+}
