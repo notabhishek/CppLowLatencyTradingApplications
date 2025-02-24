f7cea8bbba6c35b6f7377ce3309761f1b7c742d4
Abhishek Tiwari <notabhishektiwari@gmail.com>
2025-02-24 13:14:14 +0000

    init thread_example.cpp

---
+++ chapter4/thread_example.cpp
@@ -0,0 +1,23 @@
+#include "thread_utils.h"
+
+auto testFunction(int a, int b, bool sleep) {
+    std::cout << "testFunction(" << a << "," << b << ")" << std::endl;
+    std::cout << "testFunction(" << a + b << std::endl;
+    if(sleep) {
+        std::cout << "testFunction sleeping..." << std::endl;
+        using namespace std::literals::chrono_literals;
+        std::this_thread::sleep_for(5s);
+    }
+    std::cout << "testFunction done." << std::endl;
+}
+
+int main() {
+    auto t1 = createAndStartThread(-1, "testFunction1", 10, 15, false);
+    auto t2 = createAndStartThread(1, "testFunction2", 20, 25, true);
+
+    std::cout << "main waiting for threads to be done." << std::endl;
+    t1 -> join();
+    t2 -> join();
+    std::cout << "main exiting." << std::endl;
+    return 0;
+}
